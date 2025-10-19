58bfdde1bafae9bfdae6a1698a475b66f14c2f4d
VeryExcitedGremlin <oder.ct@gmail.com>
2025-10-18 18:49:11 -0700

    update styling

--- Full-Stack-Boot-Camp/Projects/vanilla-js-api-integration-project/Stock-app/stonks.js
+++ Full-Stack-Boot-Camp/Projects/vanilla-js-api-integration-project/Stock-app/stonks.js
@@ -1,32 +1,48 @@
-const portfolioCards = document.getElementById('portfolio-cards');
+const lookupForm = document.getElementById("stock-lookup");
+const lookupInput = document.getElementById("lookup-input");
+const lookupQuick = document.getElementById('quick-lookup');
 
+const portfolioCards = document.getElementById("portfolio-cards");
+
+const quickLookup = ['APPL', 'GOOGL', 'MSFT', 'TSLA', 'AMZN'];
 const cardObjects = [
-    {
-        id: 'totalValue',
-        title: 'Total Portfolio Value',
-        text: '$0.00'
-    },
-    {
-        id: 'positions',
-        title: 'Number of Positions',
-        text: '0'
-    },
-    {
-        id: 'topStock',
-        title: 'Top Performing Stock',
-        text: '-'
-    },
-    {
-        id: 'change',
-        title: "Today's Change",
-        text: '$0.00'
-    },
-]
+  {
+    id: "totalValue",
+    title: "Total Portfolio Value",
+    text: "$0.00",
+  },
+  {
+    id: "positions",
+    title: "Number of Positions",
+    text: "0",
+  },
+  {
+    id: "topStock",
+    title: "Top Performing Stock",
+    text: "-",
+  },
+  {
+    id: "change",
+    title: "Today's Change",
+    text: "$0.00",
+  },
+];
 
-function showCards() {
-    let cards = ''
-    cardObjects.forEach( (card) => {
-        const cardText = `
+function buildQuickLookup() {
+    let inner = '<span class="col-auto">Quick Search:</span><div class="col-auto">'
+    quickLookup.forEach( (quickLink) => {
+        inner += `
+            <span class='quick col-auto'>${quickLink}</span>
+        `
+    });
+    inner += '</div>'
+    lookupQuick.innerHTML = inner;
+}
+
+function buildCards() {
+  let cards = "";
+  cardObjects.forEach((card) => {
+    const cardText = `
             <div class="col-12 col-md-6 col-xl-3">
                 <div class="inner-section card" id='${card.id}'>
                     <div class="card-body">
@@ -36,9 +52,36 @@ function showCards() {
                 </div>
             </div>
         `;
-        cards += cardText;
-    });
-    portfolioCards.innerHTML = cards;
+    cards += cardText;
+  });
+  portfolioCards.innerHTML = cards;
 }
 
-showCards();
+function getData(event) {
+    event.preventDefault();
+}
+
+async function callData(access_key, symbol) {
+  const url = `http://api.marketstack.com/v2/eod?${access_key}=c37cf927e188929771585709f822c131&symbols=${symbol}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+buildQuickLookup();
+buildCards();
+
+
+
+
+//listeners
+lookupForm.querySelector('button').addEventListener('submit', getData);
+// lookupQuick.addEventListener('click', );
