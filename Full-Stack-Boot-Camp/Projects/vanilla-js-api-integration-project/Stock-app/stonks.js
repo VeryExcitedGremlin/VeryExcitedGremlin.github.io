9248b4944d83a2fe098b70e418cc260575f6c0e7
VeryExcitedGremlin <oder.ct@gmail.com>
2025-10-21 17:57:51 -0700

    show data!

--- Full-Stack-Boot-Camp/Projects/vanilla-js-api-integration-project/Stock-app/stonks.js
+++ Full-Stack-Boot-Camp/Projects/vanilla-js-api-integration-project/Stock-app/stonks.js
@@ -6,15 +6,6 @@ const showStock = document.getElementById('show-stock');
 
 const portfolioCards = document.getElementById("portfolio-cards");
 
-function quickLookup(event) {
-  const target = event.target
-  if (target.classList.contains('quick')){
-    const text = target.textContent;
-    lookupInput.value = text;
-  }
-  callAPI()
-}
-
 async function callAPI(event) {
   if (event) {
     event.preventDefault();
@@ -58,12 +49,122 @@ async function callAPI(event) {
 }
 
 function displayStock(symbol) {
-  let stockCard = createElement("div");
-  const data = localStorage.getItem(symbol);
-  const userData = localStorage.getItem(`${symbol}-user`)
-  const inner = `
+  showStock.classList.add('outer-section');
 
-  `
+  // let stockCard = createElement("div");
+
+  const data = JSON.parse(localStorage.getItem(symbol));
+
+  let userData
+  if (Object.keys(localStorage).includes(`${symbol}-user`)) {
+    userData = JSON.parse(localStorage.getItem(`${symbol}-user`));
+  }
+
+  let inner = `
+    <div id="stock-banner" class="row justify-content-around align-items-center">
+        <div class="col">
+            <div class="row justify-content-center justify-content-sm-start">
+                <h2 class="col-auto">${data.name}</h2>
+                    <div id='stock-symbol' class="dis-val col-auto mb-2">${symbol}</div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-auto">
+            <button><i class="fa-solid fa-thumbtack"></i>Add to Watchlist</button>
+        </div>
+    </div>
+    <div id="stock-info" class="row align-items-center justify-content-around">
+        
+        <div class="col-auto mb-3">
+            <h1 id='close'>${data.close}</h1>
+            <span id="change" class="">${data.close - data.open}</span>
+        </div>
+        
+        <div class="col-auto col-md-8 col-lg-6">
+            <div class="row justify-content-between">
+  `;
+
+  const info = ['open', 'high', 'low', 'volume'];
+  info.forEach( (item) => {
+    inner += `
+              <div class="col-6">
+                  <div class="row justify-content-sm-between stock-data m-1 p-1">
+                      <strong class="col-12 col-sm-auto">${capitalizeFirstLetter(item)}:</strong>
+                      <span class="col-12 col-sm-auto">${item == 'volume' ? data[item] : ('$'+data[item])}</span>
+                  </div>
+              </div>
+    `;
+  });
+
+  inner += `
+            </div>
+        </div>
+    </div>
+    <div id="portfolio-management" class="row justify-content-center justify-content-sm-start">
+        <h3 class="col-auto">Portfolio Management</h3>
+
+        <div class="row justify-content-around">
+
+            <div class="col-11 col-sm-5 dis-val">
+                <strong class="col-12 col-md-auto">Current Holdings:</strong>
+                <div id='adjust-holdings' class="row justify-content-around">
+                    <span id="less" class="adjust col-2 col-lg-2 ms-3">-</span>
+                    <span id='holdings-display' class="col-3 col-lg-3 dis-val-inner">${
+                      userData ? userData.holdings : 0
+                    }</span>
+                    <span id="more" class="adjust col-2 col-lg-2 me-3">+</span>
+                </div>
+            </div>
+
+            <div class="col-11 col-sm-5">
+                <div class="row justify-content-center dis-val">
+                    <strong class="col-12">Position Value:</strong>
+                    <span id='holdings-value' class="col-auto dis-val-inner">$${
+                      userData ? userData.holdings * data.close : "0.00"
+                    }</span>
+                </div>
+            </div>
+            
+          </div>
+
+      </div>
+  `;
+
+  showStock.innerHTML = inner;
+  document.getElementById('adjust-holdings').addEventListener('click', adjustHoldings);
+}
+
+function adjustHoldings(event) {
+  const content = event.target.textContent;
+  const symbol = document.getElementById('stock-symbol').textContent
+  const stock = JSON.parse(localStorage.getItem(symbol))
+  let userData = {}
+  if (Object.keys(localStorage).includes(`${symbol}-user`)) {
+    userData = JSON.parse(localStorage.getItem(`${symbol}-user`))
+  }
+  if (!userData.holdings) { userData.holdings = 0 }
+  if (!userData.value) { userData.value = 0 }
+
+  if (content == '+') {
+    userData.holdings += 1;
+  } else if (content == '-') { userData.holdings == 0 ? userData.holdings = 0 : userData.holdings -= 1 }
+
+  userData.value = userData.holdings * stock.close
+  document.getElementById('holdings-value').textContent = `$${userData.value}`
+  document.getElementById("holdings-display").textContent = userData.holdings;
+
+  localStorage.setItem(`${symbol}-user`, JSON.stringify(userData));
+  // console.log(symbol);
+}
+
+// Helpers
+
+function quickLookup(event) {
+  const target = event.target;
+  if (target.classList.contains("quick")) {
+    const text = target.textContent;
+    lookupInput.value = text;
+  }
+  callAPI();
 }
 
 function getToday() {
@@ -83,6 +184,13 @@ function getToday() {
   return today
 }
 
+function capitalizeFirstLetter(str) {
+  const first = str.charAt(0);
+  const rest = str.substring(1);
+  const firstCap = first.toUpperCase();
+
+  return firstCap + rest
+}
 //Build page
 const quickLookupList = ["AAPL", "GOOGL", "MSFT", "TSLA", "AMZN"];
 const cardObjects = [
