c282f48703b8a1a8c3fd0f8583f1b5040ac512a6
VeryExcitedGremlin <oder.ct@gmail.com>
2025-10-27 16:36:47 -0700

    make api key saveable

--- Full-Stack-Boot-Camp/Projects/vanilla-js-api-integration-project/Stock-app/stonks.js
+++ Full-Stack-Boot-Camp/Projects/vanilla-js-api-integration-project/Stock-app/stonks.js
@@ -1,7 +1,9 @@
+// const { createElement } = require("react");
+
 const apiKeyInput = document.getElementById("api-key");
 const enterKey = document.getElementById("enter-key");
 
-const lookupForm = document.getElementById("stock-lookup");
+// const lookupForm = document.getElementById("stock-lookup");
 const lookupInput = document.getElementById("lookup-input");
 const lookupQuickSection = document.getElementById("quick-lookup-section");
 const showStock = document.getElementById("show-stock");
@@ -10,78 +12,100 @@ const lookupWarning = document.getElementById("lookup-warning");
 const portfolioCards = document.getElementById("portfolio-cards");
 
 async function callAPI(event) {
+  // const apiKeyInput = document.getElementById("api-key");
+  // if (apiKeyInput) {
   // apiKeyInput.classList.remove("border-danger-subtle");
+  // }
   lookupInput.classList.remove("border-danger-subtle");
   if (event) {
     event.preventDefault();
   }
-  const access_key = apiKeyInput.value;
-  // const access_key = JSON.parse(localStorage.getItem('API'));
+  // const access_key = apiKeyInput.value;
+  const access_key = JSON.parse(localStorage.getItem('API'));
   const symbol = lookupInput.value;
+  // console.log(!symbol.length);
   const today = getToday();
-  if (
+
+  if (!access_key && !symbol.length) {
+    // enterKey.innerHTML = `
+    //   <input class="col-6 mb-2 border-danger-subtle" type="text" id="api-key" placeholder="Enter API key" />
+    //   <button id='api-submit' class="col-4 col-md-3 mb-2" onclick="keySubmit(event)">Submit</button>
+    // `;
+    apiKeyInput.classList.add("border-danger-subtle");
+    lookupInput.classList.add("border-danger-subtle");
+    lookupWarning.textContent = "Check stock symbol and API key";
+  } else if (!access_key) {
+    // enterKey.innerHTML = `
+    //   <input class="col-6 mb-2 border-danger-subtle" type="text" id="api-key" placeholder="Enter API key" />
+    //   <button id='api-submit' class="col-4 col-md-3 mb-2" onclick="keySubmit(event)">Submit</button>
+    // `;
+    apiKeyInput.classList.add("border-danger-subtle");
+    lookupWarning.textContent = "Check API key";
+  } else if (!symbol.length) {
+    lookupInput.classList.add("border-danger-subtle");
+    lookupWarning.textContent = "Check stock symbol";
+    // console.log('hit !symbol.length')
+  } else if (
     !Object.keys(localStorage).includes(symbol) ||
     JSON.parse(localStorage.getItem(symbol)).date !== today
   ) {
-    if (access_key && symbol) {
-      const url = `https://api.marketstack.com/v2/eod?access_key=${access_key}&symbols=${symbol}&date_from=${today}`;
-      // return url
-      try {
-        const response = await fetch(url);
+    // if (access_key && symbol) {
+    const url = `https://api.marketstack.com/v2/eod?access_key=${access_key}&symbols=${symbol}&date_from=${today}`;
+    // return url
+    try {
+      const response = await fetch(url);
 
-        if (response.ok) {
-          const result = await response.json();
-          // console.log(result)
-          const stockObject = result.data[0];
-          console.log(stockObject);
-          const change = (stockObject.close - stockObject.open).toFixed(2);
-          const percent = (change / stockObject.open).toFixed(2);
+      if (response.ok) {
+        const result = await response.json();
+        // console.log(result)
+        const stockObject = result.data[0];
+        console.log(stockObject);
+        const change = (stockObject.close - stockObject.open).toFixed(2);
+        const percent = (change / stockObject.open).toFixed(2);
 
-          localStorage.setItem(
-            symbol,
-            JSON.stringify({
-              open: stockObject.open,
-              high: stockObject.high,
-              low: stockObject.low,
-              close: stockObject.close,
-              name: stockObject.name,
-              volume: stockObject.volume,
-              date: today,
-              change: change,
-              percent: percent,
-            })
-          );
-        }
-
-        throw new Error(`Response status: ${response.status}`);
-      } catch (error) {
-        console.error(error.message);
+        localStorage.setItem(
+          symbol,
+          JSON.stringify({
+            open: stockObject.open.toFixed(2),
+            high: stockObject.high.toFixed(2),
+            low: stockObject.low.toFixed(2),
+            close: stockObject.close.toFixed(2),
+            name: stockObject.name,
+            volume: stockObject.volume,
+            date: today,
+            change: change,
+            percent: percent,
+          })
+        );
       }
-    } else {
-      // console.log('something missing');
-      if (!access_key && !symbol) {
-        apiKeyInput.classList.add("border-danger-subtle");
-        lookupInput.classList.add("border-danger-subtle");
-        lookupWarning.textContent = "Missing both stock symbol and API key";
-      } else if (!access_key) {
-        apiKeyInput.classList.add("border-danger-subtle");
-        lookupWarning.textContent = "Missing API key";
-      } else if (!symbol) {
-        lookupInput.classList.add("border-danger-subtle");
-        lookupWarning.textContent = "Missing stock symbol";
-      }
+
+      throw new Error(`Response status: ${response.status}`);
+    } catch (error) {
+      console.error(error.message);
     }
+    // } else {
+    //   // console.log('something missing');
+
+    // }
   } else {
     console.log(`already have ${symbol} for today`);
-  }
 
+    if (localStorage.getItem(symbol)) {
+      displayStock(symbol);
+    } else {
+      enterKey.innerHTML = `
+        <input class="col-6 mb-2 border-danger-subtle" type="text" id="api-key" placeholder="Enter API key" />
+        <button id='api-submit' class="col-4 col-md-3 mb-2" onclick="keySubmit(event)">Submit</button>
+      `;
+      document.getElementById("api-key").value = access_key;
+      // apiKeyInput.classList.add("border-danger-subtle");
+      lookupInput.classList.add("border-danger-subtle");
+      lookupWarning.textContent = "Check stock symbol and API key; no saved data";
+      showStock.innerHTML = "";
+    }
+  }
   if (localStorage.getItem(symbol)) {
     displayStock(symbol);
-  } else {
-    apiKeyInput.classList.add("border-danger-subtle");
-    lookupInput.classList.add("border-danger-subtle");
-    lookupWarning.textContent = "Check stock symbol and API key; no saved data";
-    showStock.innerHTML = "";
   }
 }
 
@@ -93,10 +117,15 @@ function displayStock(symbol) {
   const data = JSON.parse(localStorage.getItem(symbol));
   let old = false;
   if (data.date != getToday()) {
-    apiKeyInput.classList.add("border-danger-subtle");
+    const access_key = JSON.parse(localStorage.getItem("API"));
+    enterKey.innerHTML = `
+      <input class="col-6 mb-2 border-danger-subtle" type="text" id="api-key" placeholder="Enter API key" />
+      <button id='api-submit' class="col-4 col-md-3 mb-2" onclick="keySubmit(event)">Submit</button>
+    `;
+    document.getElementById("api-key").value = access_key;
     lookupInput.classList.add("border-danger-subtle");
     lookupWarning.textContent =
-      "Check stock symbol and API key; showing saved data";
+      "Check API key or internet connection; showing saved data";
     old = true;
   }
 
@@ -151,7 +180,9 @@ function displayStock(symbol) {
               </div>
     `;
   });
-
+  userData
+    ? (userData.value = (userData.holdings * data.close).toFixed(2))
+    : "";
   inner += `
             </div>
         </div>
@@ -176,7 +207,7 @@ function displayStock(symbol) {
                 <div class="row justify-content-center dis-val">
                     <strong class="col-12">Position Value:</strong>
                     <span id='holdings-value' class="col-auto dis-val-inner">$${
-                      userData ? userData.holdings * data.close : "0.00"
+                      userData ? userData.value : "0.00"
                     }</span>
                 </div>
             </div>
@@ -193,7 +224,9 @@ function displayStock(symbol) {
 }
 
 function adjustHoldings(event) {
+  const holdingsDisplay = document.getElementById("holdings-display");
   const content = event.target.textContent;
+  // console.log(Number.isNaN(parseFloat(content)))
   const symbol = document.getElementById("stock-symbol").textContent;
   const stock = JSON.parse(localStorage.getItem(symbol));
   let userData = {};
@@ -211,11 +244,17 @@ function adjustHoldings(event) {
     userData.holdings += 1;
   } else if (content == "-") {
     userData.holdings == 0 ? (userData.holdings = 0) : (userData.holdings -= 1);
-  }
-
-  userData.value = (userData.holdings * stock.close).toFixed(2);
+  } 
+  // else if (!Number.isNaN(parseFloat(content))) {
+  //   // console.log('inside')
+  //   const updateHoldings = document.createElement('input');
+  //   updateHoldings.setAttribute('type', 'number');
+  //   updateHoldings.setAttribute("placeholder", content);
+  //   holdingsDisplay.value = updateHoldings;
+  // }
+    userData.value = (userData.holdings * stock.close).toFixed(2);
   document.getElementById("holdings-value").textContent = `$${userData.value}`;
-  document.getElementById("holdings-display").textContent = userData.holdings;
+  holdingsDisplay.textContent = userData.holdings;
 
   localStorage.setItem(`${symbol}-user`, JSON.stringify(userData));
   // console.log(symbol);
@@ -223,17 +262,21 @@ function adjustHoldings(event) {
 
 // Helpers
 
-// function keySubmit(event) {
-//   event.preventDefault();
-//   localStorage.setItem(
-//     "API",
-//     JSON.stringify(document.getElementById("api-key").value)
-//   );
+function calculatePosition() {
+  const keys = Object.keys(localStorage).filter( (key) => key.includes('-user') )
+  const showVal = document.querySelectorAll('.total-value');
+  let total = 0
+  keys.forEach( (key) => total += parseFloat(JSON.parse(localStorage.getItem(key)).value) )
+  console.log(total);
+}
 
-//   enterKey.innerHTML = `<span class='col-6 mb-2'>${JSON.parse(
-//     localStorage.getItem("API")
-//   )}</span>`;
-// }
+function keySubmit(event) {
+  event.preventDefault();
+  const key = document.getElementById("api-key").value;
+  localStorage.setItem("API", JSON.stringify(key));
+
+  enterKey.innerHTML = `<span class='col-auto mb-2'>${key}</span>`;
+}
 
 function quickLookup(event) {
   const target = event.target;
@@ -277,6 +320,7 @@ const quickLookupList = ["AAPL", "GOOGL", "MSFT", "TSLA", "AMZN"];
 const cardObjects = [
   {
     id: "totalValue",
+    class: 'total-value',
     title: "Total Portfolio Value",
     text: "$0.00",
   },
@@ -313,7 +357,7 @@ function buildCards() {
   let cards = "";
   cardObjects.forEach((card) => {
     const cardText = `
-            <div class="col-12 col-md-6 col-xl-3">
+            <div class="col-12 col-md-6 col-xl-3 ${card.class}">
                 <div class="inner-section card" id='${card.id}'>
                     <div class="card-body">
                         <h6 class="card-title">${card.title}</h6>
@@ -327,22 +371,22 @@ function buildCards() {
   portfolioCards.innerHTML = cards;
 }
 
-// function showKeySubmit() {
-//   if (Object.keys(localStorage).includes('API')) {
-//     enterKey.innerHTML = `<span class='col-6 mb-2'>${JSON.parse(localStorage.getItem('API'))}</span>`
-//   } else {
-//     enterKey.innerHTML = `
-//       <input class="col-6 mb-2" type="text" id="api-key" placeholder="Enter API key" />
-//       <button id='api-submit' class="col-4 col-md-3 mb-2">Submit</button>
-//     `
-//     document.getElementById('api-submit').addEventListener('click', keySubmit);
-//   }
-// }
+function showKeySubmit() {
+  const key = JSON.parse(localStorage.getItem('API'));
+  if (key) {
+    enterKey.innerHTML = `<span class='col-auto mb-2'>${key}</span>`
+  } else {
+    enterKey.innerHTML = `
+      <input class="col-6 mb-2" type="text" id="api-key" placeholder="Enter API key" />
+      <button id='api-submit' class="col-4 col-md-3 mb-2" onclick="keySubmit(event)">Submit</button>
+    `
+  }
+}
 
-// showKeySubmit();
+showKeySubmit();
 buildQuickLookup();
 buildCards();
 
 //listeners
-lookupForm.addEventListener("submit", callAPI);
+// lookupForm.addEventListener("submit", callAPI);
 lookupQuickSection.addEventListener("click", quickLookup);
