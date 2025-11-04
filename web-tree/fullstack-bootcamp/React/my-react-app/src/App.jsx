da1c59641430bb203378764dd2f23ef17ed7f375
VeryExcitedGremlin <oder.ct@gmail.com>
2025-11-03 16:38:20 -0800

    yay routes

--- web-tree/fullstack-bootcamp/React/my-react-app/src/App.jsx
+++ web-tree/fullstack-bootcamp/React/my-react-app/src/App.jsx
@@ -4,6 +4,7 @@
 import { Route, Routes } from "react-router";
 import DisplayHello from "./components/module-47-react-intro/display-47/Display47";
 import DisplayCounters from "./components/module-49-state-management/display-49/Display49";
+import { PersonalFooter } from "./components/Footer/personal-footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/47" element={<DisplayHello />} />
         <Route path="/49" element={<DisplayCounters />} />
       </Routes>
+      <PersonalFooter />
     </>
   );
 }
