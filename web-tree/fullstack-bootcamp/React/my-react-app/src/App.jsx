1a6974f49197f326c1e4f6d56bf79fb3b517d6c6
VeryExcitedGremlin <oder.ct@gmail.com>
2025-11-03 16:32:54 -0800

    make routes work

--- web-tree/fullstack-bootcamp/React/my-react-app/src/App.jsx
+++ web-tree/fullstack-bootcamp/React/my-react-app/src/App.jsx
@@ -1,15 +1,17 @@
 // import HelloWorldInline from "./components/module-47-react-intro/HelloWorld/HelloWorld";
 // import { HelloWorld } from "./components/module-47-react-intro/HelloWorld/HelloWorld";
 // import { StaticCounter,InteractiveCounter,ToggleSwitch } from "./components/module-49-state-management/Counter/counter";
-
+import { Route, Routes } from "react-router";
 import DisplayHello from "./components/module-47-react-intro/display-47/Display47";
 import DisplayCounters from "./components/module-49-state-management/display-49/Display49";
 
 function App() {
   return (
     <>
-      <DisplayHello />
-      <DisplayCounters />
+      <Routes>
+        <Route path="/47" element={<DisplayHello />} />
+        <Route path="/49" element={<DisplayCounters />} />
+      </Routes>
     </>
   );
 }
